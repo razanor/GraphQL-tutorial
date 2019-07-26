d20e48e645b6825c0ef0f7aa369043cade0d397b
razanor <nazara.re22@gmail.com>
2019-07-26 16:52:46 +0300

    Ability to vote for news added

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -11,6 +11,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -28,7 +38,9 @@ class LinkList extends Component {
     
           return (
             <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+              {linksToRender.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
+              ))}
             </div>
           )
         }}
