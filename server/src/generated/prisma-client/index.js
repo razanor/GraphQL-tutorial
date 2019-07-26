81a56280c54b2930f084bab4a2c926d09d82b57e
razanor <nazara.re22@gmail.com>
2019-07-26 13:19:02 +0300

    Prisma client configured

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,21 +20,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: `https://eu1.prisma.sh/nazarii-repak-ebaefa/hackernews-react-apollo/dev`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
