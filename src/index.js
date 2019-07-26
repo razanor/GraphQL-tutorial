9cdb8d86d7169ecd0b8d4c0eff72e6305af814a3
razanor <nazara.re22@gmail.com>
2019-07-26 16:16:42 +0300

    Authentication added

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 
 // 2
@@ -17,9 +19,19 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
